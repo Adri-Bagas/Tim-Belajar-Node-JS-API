eef2e44758e68bb832a9d12b2746b267228e67af
yyypluto <mfarrasashshiddiqi@gmail.com>
2024-10-31 09:21:17 +0700

    Pengumpulan FARRAS

--- Model/pokemon.model.js
+++ Model/pokemon.model.js
@@ -1,8 +1,7 @@
-
-
 exports.Pokemon = {
-    createTable: function(conn)  {
-        conn.query(`
+  createTable: function (conn) {
+    conn.query(
+      `
             CREATE TABLE IF NOT EXISTS pokemons (
                 id int(11) NOT NULL auto_increment,
                 name varchar(255) not null,
@@ -10,41 +9,67 @@ exports.Pokemon = {
                 img varchar(255) not null,
                 PRIMARY KEY (id)
             );
-        `, function (err, result){
-            if (err) throw err;
-            console.log("Table Pokemon Created!")
-        })
-    },
-    all: function(conn)  {
-        return new Promise((resolve, reject) => {
-            conn.query(`
+        `,
+      function (err, result) {
+        if (err) throw err;
+        console.log("Table Pokemon Created!");
+      }
+    );
+  },
+  all: function (conn) {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
                 SELECT * FROM pokemons;
-            `, function (err, result, fields){
-                if (err) return resolve([null, err]);
+            `,
+        function (err, result, fields) {
+          if (err) return resolve([null, err]);
 
-                return resolve([result, err]);
-            })
-        })
-    },
-    create: (conn, name, element, img) => {
-
-        return new Promise((resolve, reject) => {
-            conn.query(`
+          return resolve([result, err]);
+        }
+      );
+    });
+  },
+  create: (conn, name, element, img) => {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
                 INSERT INTO pokemons (name, element, img) VALUES ('${name}', '${element}', '${img}');
-            `, function (err, result){
-                if (err) resolve(err);
-                console.log(`Success create ${name}!`)
-                resolve(null)
-            })
-        })
-
-
-    },
-    delete: (conn, id) => {
-        // Bikin Sendiri!
-    },
-    update: (conn, id, name, element, img) => {
-         // Bikin Sendiri!
-    },
-}
-
+            `,
+        function (err, result) {
+          if (err) resolve(err);
+          console.log(`Success create ${name}!`);
+          resolve(null);
+        }
+      );
+    });
+  },
+  delete: (conn, id) => {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
+                DELETE FROM pokemons WHERE id=${id};
+            `,
+        function (err, result) {
+          if (err) resolve(err);
+          console.log(`Success delete pokemon with id: ${id}!`);
+          resolve(null);
+        }
+      );
+    });
+  },
+  update: (conn, id, name, element, img) => {
+    return new Promise((resolve, reject) => {
+      conn.query(
+        `
+                UPDATE pokemons SET name='${name}', element='${element}', img='${img}';
+            `,
+        function (err, result) {
+          if (err) resolve(err);
+          console.log(`Success udpate pokemon with id: ${id}!`);
+          resolve(null);
+        }
+      );
+    });
+  },
+};
--- Utils/utils-function.js
+++ Utils/utils-function.js
@@ -1,22 +1,22 @@
 exports.parseBody = function (req) {
-    return new Promise((resolve, reject) => {
-        if (req.method === "POST") {
-            let body = "";
-            req.on("data", (chunk) => {
-                body += chunk.toString();
-            });
-            req.on("end", () => {
-                try {
-                    const parsedData = JSON.parse(body);
-                    resolve([parsedData, null]);
-                } catch (error) {
-                    console.error("Failed to parse JSON:", error);
-                    resolve ([null, error]);
-                }
-            });
-        } else {
-            resolve( [ null, "Method Not Allowed!"])
+  return new Promise((resolve, reject) => {
+    console.log("Do parsing with method " + req.method);
+    if (req.method == "POST" || req.method == "PUT" || req.method == "DELETE") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parsedData = JSON.parse(body);
+          resolve([parsedData, null]);
+        } catch (error) {
+          console.error("Failed to parse JSON:", error);
+          resolve([null, error]);
         }
-    })
-    
+      });
+    } else {
+      resolve([null, "Method Not Allowed!"]);
+    }
+  });
 };
